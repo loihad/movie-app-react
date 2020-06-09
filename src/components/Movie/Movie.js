0333a6e97abfe8c5dd13ff20c5d6aa6a2bc2ffbf
haloi <hideonstorms@gmail.com>
2020-06-09 21:12:36 +0700

    source layout

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { API_URL, API_KEY } from "../../config";
+// import Navigation from "../elements/Navigation";
+// import MovieInfo from "../elements/MovieInfo";
+// import MovieInfoBar from "../elements/MovieInfoBar";
+// import FourColGrid from "../elements/FourColGrid";
+// // import Actor from "../elements/Actor";
+// import Spinner from "../elements/Spinner";
 
 export default class Movie extends Component {
   render() {
-    return <div className="rmdb-header">movie this is</div>;
+    return <div className="rmdb-header">movie this is k</div>;
   }
 }
---
+++ src/components/elements/Navigation/Navigation.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+const Navigation = (props) => {
+  return <h1>navigation</h1>;
+};
